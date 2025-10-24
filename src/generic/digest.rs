7123188e2b43168979c525c0db3e06adbdb7638b
Miaourt <1571367+Miaourt@users.noreply.github.com>
2025-10-24 14:47:59 +0200

    Use constant_time_eq on BabDigest

--- src/generic/digest.rs
+++ src/generic/digest.rs
@@ -1,6 +1,6 @@
 use zeroize::Zeroize;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+#[derive(Eq, PartialOrd, Ord, Clone, Debug)]
 #[repr(transparent)]
 pub struct BabDigest<const WIDTH: usize>(pub(crate) [u8; WIDTH]);
 
@@ -13,6 +13,12 @@ impl<const WIDTH: usize> From<[u8; WIDTH]> for BabDigest<WIDTH> {
 impl<const WIDTH: usize> From<BabDigest<WIDTH>> for [u8; WIDTH] {
     fn from(value: BabDigest<WIDTH>) -> Self {
         value.0
+    }
+}
+
+impl<const WIDTH: usize> PartialEq for BabDigest<WIDTH> {
+    fn eq(&self, other: &Self) -> bool {
+        constant_time_eq::constant_time_eq_n(&self.0, &other.0)
     }
 }
 
